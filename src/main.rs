dfd732cde06cea0d2d777fccd7dd104ed36d6efa
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2015-10-22 23:44:28 +0600

    add std:thread for sleep in while loop

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,25 @@ extern crate dbus;
 use dbus::{Connection, BusType, NameFlag};
 use dbus::tree::Signal;
 
+use std::thread;
+
 fn main() {
     let connection = Connection::get_private(BusType::Session).unwrap();
 
     let rule = "ping from rustabus".to_string();
 
-    let mut message = dbus::Message::new_signal(
-        "/com/github/kovetskiy/rustabus",
-        "com.github.kovetskiy.rustabus",
-        "Ping"
-    ).unwrap();
 
-    message.append_items(&[dbus::MessageItem::Str(rule.to_string())]);
+    while true {
+        let mut message = dbus::Message::new_signal(
+            "/com/github/kovetskiy/rustabus",
+            "com.github.kovetskiy.rustabus",
+            "Ping"
+        ).unwrap();
 
-    connection.send(message);
+        message.append_items(&[dbus::MessageItem::Str(rule.to_string())]);
+
+        connection.send(message);
+
+        thread::sleep_ms(1000);
+    }
 }
